966ec034014180c7a9768d59aa9605934ba41df6
Pravat Bhusal <pravat.bhusal@gmail.com>
2017-09-26 22:20:17 -0500

    Make the game scroll bar start at the middle

--- playgame.php
+++ playgame.php
@@ -159,4 +159,12 @@
     <!--jquery and bootstrap-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+	<script>
+	//make the scrollbar start at the middle
+	  window.scrollTo(
+		(document.body.offsetWidth -window.innerWidth )/2,
+		(document.body.offsetHeight-window.innerHeight)/2
+	  );
+	</script>
 </html>
