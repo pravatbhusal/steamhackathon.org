a5b23e0e9143217bf5b8a331e871287b83320b56
Pravat Bhusal <pravat.bhusal@gmail.com>
2017-10-07 18:23:50 -0500

    Change reviewgame.php title

--- reviewgame.php
+++ reviewgame.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1"> 
         <meta name="description" content=""> 
         <meta name="author" content=""> 
-        <title>Play Game</title>    
+        <title>Review Game</title>    
 
 		<!--style.css, favcon, bootstrap-->
         <link href="style.css" rel="stylesheet">       
